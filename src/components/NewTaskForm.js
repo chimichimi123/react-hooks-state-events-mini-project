afb4ad732466060ec46c948356546348352fb4ad
Christopher Cantu <chrischimi02@gmail.com>
2024-04-15 00:24:35 -0500

    complete lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,44 @@
-import React from "react";
+import { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const initalData = { text: "", category: "Code" };
+  const [newTask, setNewTask] = useState(initalData);
+
+  const handleChange = (e) => {
+    setNewTask({ ...newTask, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onTaskFormSubmit(newTask);
+    console.log(newTask);
+    setNewTask(initalData);
+  };
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          onChange={handleChange}
+          value={newTask.text}
+          type="text"
+          name="text"
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select
+          value={newTask.category}
+          name="category"
+          type="text"
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
